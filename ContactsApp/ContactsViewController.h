5fe0920949510387af8934e81824955760c37731
Riaan <riaan@Meran-iMac.local>
2015-05-13 19:59:10 +0200

    Some more navigation

---
+++ ContactsApp/ContactsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactsViewController.h
+//  ContactsApp
+//
+//  Created by Riaan on 2015/05/13.
+//  Copyright (c) 2015 Riaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactsViewController : UITableViewController
+
+@end
---
+++ ContactsApp/ContactsViewController.m
@@ -0,0 +1,93 @@
+//
+//  ContactsViewController.m
+//  ContactsApp
+//
+//  Created by Riaan on 2015/05/13.
+//  Copyright (c) 2015 Riaan. All rights reserved.
+//
+
+#import "ContactsViewController.h"
+
+@interface ContactsViewController ()
+@property (nonatomic) NSArray *contacts;
+@end
+
+@implementation ContactsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.contacts = @[@"First", @"Second"];
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.contacts.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"contactsCell" forIndexPath:indexPath];
+    cell.detailTextLabel.text = @"...";
+    cell.textLabel.text = [self.contacts objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+/*
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if(indexPath.row % 2 ==0){
+        [self performSegueWithIdentifier:@"contactPushSegue" sender:self];
+    }
+}
+ */
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
